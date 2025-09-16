d1ed4920a1809a5563cda88efdc085404ecd0e9f
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-09-15 17:15:28 -0700

    added method to get champion mastery + made champion class

--- get_acc.py
+++ get_acc.py
@@ -6,6 +6,14 @@ import os
 load_dotenv()
 api_key = os.getenv("RIOT_API")
 
+class Champ_Mastery:
+    def __init__(self, response: dict):
+        self.json_info = response
+    
+    def print(self):
+        for key in self.json_info:
+            print(f"{key}: {self.json_info[key]}")
+
 class Riot_Acc:
     def __init__(self, api_key, user, tag):
         acc_url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{user}/{tag}"
@@ -14,13 +22,26 @@ class Riot_Acc:
         }
         response = requests.get(acc_url, headers=headers)
         mp = json.loads(response.text) 
+        self.api_key = api_key
         self.puuid = mp["puuid"]
         self.gameName = mp["gameName"]
         self.tagLine = mp["tagLine"]
+        self.champList = []
     
     def get_info(self):
         print(f"puuid: {self.puuid}\nusername: {self.gameName}#{self.tagLine}")
-
-test.get_info()
-
     
+    def get_champ_mastery(self):
+        # https://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json
+        url = f"https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{self.puuid}"
+        headers = {
+            "X-Riot-Token": api_key
+        }
+        response = requests.get(url, headers=headers)
+        mp = json.loads(response.text)
+        
+        for key in mp:
+            self.champList.append(Champ_Mastery(key))
+        
+        for champ in self.champList:
+            print(champ.print())
