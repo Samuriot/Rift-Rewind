b9b87fcfa16ae3529b62a56a14afdbc6450175f3
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-09-15 19:31:35 -0700

    created champion class to link champ ids with champ info

---
+++ champion.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+class Champion:
+    def __init__(self, name, title, blurb, info, tags, partype, stats):
+        self.name = name
+        self.title = title
+        self.description = blurb
+        self.info = info
+        self.tags = tags
+        self.partype = partype
+        self.stats = stats
+        self.image_url = f"https://ddragon.leagueoflegends.com/cdn/15.18.1/img/champion/{self.name}.png"
+    
+    def __str__(self):
+        return self.name
+
+def create_champ_list():
+    url = "https://ddragon.leagueoflegends.com/cdn/15.18.1/data/en_US/champion.json"
+    champ_list = requests.get(url)
+    champ_list = json.loads(champ_list.text)
+    all_champs = {}
+
+    for champion, desc in champ_list['data'].items():
+        all_champs[desc["key"]] = Champion(desc["name"], desc["title"], desc["blurb"], desc["info"], desc["tags"], desc["partype"], desc["stats"])
+
+    for key, val in all_champs.items():
+        print(f"{key}: {val} - {val.title}")
+
