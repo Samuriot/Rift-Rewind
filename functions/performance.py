d3b1c1b83148501974d05698f81eac7132bbd705
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-09-21 20:36:56 -0700

    added ObjectivePerformance class to parse Challenges part of JSON (commented lines had issues with parsing)

--- functions/performance.py
+++ functions/performance.py
@@ -5,7 +5,7 @@ class Performance(ABC):
     @abstractmethod
     def load_stats(self):
         pass
-
+    
 class EconomyPerformance(Performance):
     def __init__(self, json_file):
         self.json_file = json_file
@@ -18,6 +18,7 @@ class EconomyPerformance(Performance):
         self.neutralMinionsKilled = self.json_file["neutralMinionsKilled"]
         self.totalHeal = self.json_file["totalHeal"]
         self.totalUnitsHealed = self.json_file["totalUnitsHealed"]
+        self.goldPerMin = self.json_file["challenges"]["goldPerMinute"]
         
     def get_gold(self):
         return {
@@ -61,6 +62,10 @@ class CombatPerformance(Performance):
         self.wardsPlaced = self.json_file["wardsPlaced"]
         self.wardsKilled = self.json_file["wardsKilled"]
         self.visionWardsBoughtInGame = self.json_file["visionWardsBoughtInGame"]
+        self.damagerPerMinute = self.json_file["challenges"]["damagePerMinute"]
+        self.percentDamageTaken = self.json_file["challenges"]["damageTakenOnTeamPercentage"]
+        self.numEnemyChampCC = self.json_file["challenges"]["enemyChampionImmobilizations"]
+        self.killWithAllyCC = self.json_file["challenges"]["immobilizeAndKillWithAlly"]
         
     def get_damage_dealt(self):
         return {
@@ -86,6 +91,74 @@ class CombatPerformance(Performance):
     def get_cc_time(self):
         return self.timeEnemySpentControlled
 
+class ObjectivePerformance(Performance):
+    def __init__(self, json_file):
+        self.json_file = json_file
+        self.load_stats()
+    
+    def load_stats(self):
+        self.baronKills = self.json_file["baronKills"]
+        self.challengesDict = self.json_file["challenges"]
+        # could potentially use this for something like: "You took advantage of Fire Dragon Soul By Picking Up x Scales"
+        self.infernalScalePickups = self.challengesDict["InfernalScalePickup"]
+        self.baronTakedowns = self.challengesDict["baronTakedowns"]
+        self.blastConesOnEnemy = self.challengesDict["blastConeOppositeOpponentCount"]
+        self.buffsStole = self.challengesDict["buffsStolen"]
+        self.supportItemTimeComplete = self.challengesDict["completeSupportQuestInTime"]
+        # self.controlWardUptimeInEnemyJg = self.challengesDict["controlWardTimeCoverageInRiverOrEnemyHalf"]
+        self.numControlWards = self.challengesDict["controlWardsPlaced"]
+        self.hardSkillShotsDodged = self.challengesDict["dodgeSkillShotsSmallWindow"]
+        self.dragonTakedowns = self.challengesDict["dragonTakedowns"]
+        # self.earliestDragKillTime = self.challengesDict["earliestDragonTakedown"]
+        self.enemyJungleCampsStolen = self.challengesDict["enemyJungleMonsterKills"]
+        self.killsAgainstElderDragBuff = self.challengesDict["elderDragonKillsWithOpposingSoul"]
+        self.multiElderDragKills = self.challengesDict["elderDragonMultikills"]
+        self.epicMonsterKillsNearEnemyJg = self.challengesDict["epicMonsterKillsNearEnemyJungler"]
+        self.quickEpicMonsterKillsOnTempo = self.challengesDict["epicMonsterKillsWithin30SecondsOfSpawn"]
+        self.epicMonsterSteals = self.challengesDict["epicMonsterSteals"]
+        self.epicMonsterStealsNoSmite = self.challengesDict["epicMonsterStolenWithoutSmite"]
+        self.firstTurretsKilled = self.challengesDict["firstTurretKilled"]
+        self.flawlessAces = self.challengesDict["flawlessAces"]
+        self.ace = self.challengesDict["fullTeamTakedown"]
+        # self.earlyJgGankKills = self.challengesDict["getTakedownsInAllLanesEarlyJungleAsLaner"]
+        self.hadOpenNexus = self.challengesDict["hadOpenNexus"]
+        self.bronzeBushKills = self.challengesDict["killAfterHiddenWithAlly"]
+        self.killsNearEnemyTurret = self.challengesDict["killsNearEnemyTurret"]
+        self.killsUnderOwnTurret = self.challengesDict["killsUnderOwnTurret"]
+        self.knockEnemyIntoTeamAndKill = self.challengesDict["knockEnemyIntoTeamAndKill"]
+        self.landSkillShotsEarlyGame = self.challengesDict["landSkillShotsEarlyGame"]
+        self.laneMinionsFirst10Minutes = self.challengesDict["laneMinionsFirst10Minutes"]
+        # self.maxCsAdvantageOnLaner = self.challengesDict["maxCsAdvantageOnLaneOpponent"]
+        self.maxKillDeficit = self.challengesDict["maxKillDeficit"]
+        # self.maxLevelLeadLaneOpponent = self.challengesDict["maxLevelLeadLaneOpponent"]
+        # idk what this means "in time"
+        self.mejaisFullStackInTime = self.challengesDict["mejaisFullStackInTime"]
+        self.multiKillOneSpell = self.challengesDict["multiKillOneSpell"]
+        self.multiTurretRiftHeraldCount = self.challengesDict["multiTurretRiftHeraldCount"]
+        self.multiKills = self.challengesDict["multikills"]
+        self.multiKillsAfterFlash = self.challengesDict["multikillsAfterAggressiveFlash"]
+        self.outnumberedKills = self.challengesDict["outnumberedKills"]
+        self.picksWithAlly = self.challengesDict["pickKillWithAlly"]
+        self.quickFirstTurret = self.challengesDict["quickFirstTurret"]
+        self.quickSoloKills = self.challengesDict["quickSoloKills"]
+        self.saveAllyFromDeath = self.challengesDict["saveAllyFromDeath"]
+        self.scuttleCrabs = self.challengesDict["scuttleCrabKills"]
+        self.numSkillshotDodged = self.challengesDict["skillshotsDodged"]
+        self.numLandedSkillshot = self.challengesDict["skillshotsHit"]
+        self.soloKills = self.challengesDict["soloKills"]
+        self.survivedThreeCcInFight = self.challengesDict["survivedThreeImmobilizesInFight"]
+        self.killsInAlcove = self.challengesDict["takedownsInAlcove"]
+        self.killsInFountain = self.challengesDict["takedownsInEnemyFountain"]
+        self.numTurretPlates = self.challengesDict["turretPlatesTaken"]
+        self.numTurretsKilled = self.challengesDict["turretTakedowns"]
+        self.numTurretsWithRiftHerald = self.challengesDict["turretsTakenWithRiftHerald"]
+        self.numWardsKilled = self.challengesDict["wardTakedowns"]
+    
+    def get_nonzero_stats(self):
+        self.nonzerolist = []
+        for key, val in self.challengesDict.items():
+            if val != 0:
+                self.nonzerolist.append({key: val})
 
 # Player_Performance class which represents an individual's performance across a game of LoL
 # used by Riot_Acc and Match_Game classes via composition
@@ -96,14 +169,16 @@ class Player_Performance(Performance):
         self.user = user
         self.json_file = dump
         self.role = self.get_role(role)
-        self.load_stats()
         self.champion = self.json_file["championId"]
+        self.load_stats()
+        pass
         
     # load_stats method will parse self.json_file for information from Riot API
     # TODO: WIP currently, need to add more areas to parse
     def load_stats(self):
         self.combat_performance = CombatPerformance(self.json_file)
         self.economy_performance = EconomyPerformance(self.json_file)
+        self.objective_performance = ObjectivePerformance(self.json_file)
     
     # __str__ getter method
     def __str__(self):
